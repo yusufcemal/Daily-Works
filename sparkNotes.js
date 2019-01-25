57263c388568c88819cb6b3d6f1ae7062f449e66
yusufcemal <yusuf.celebi@useinsider.com>
2019-01-25 19:12:20 +0300

    Add big data notes

---
+++ sparkNotes.js
@@ -0,0 +1,11 @@
+/*
+Spark Tutorial Notes 
+ * Hadoop is based on the concept of batch processing where the processing happens of blocks of data that have already stored
+ * Spark process the data in real time faster than Hadoop 
+ * Multiple Format Support -> such as Parquet, JSON, Hive and Cassandra apart from the usual formats as text files, CSV, RDMS
+ * The Data Source API provides a pluggable mechanism for accessing structured data though Spark SQL
+ * Spark's MLlib is the machine learning component which is handy when it comes to big data processing 
+ * RDD and DAG 
+ * Hadoop HDFS -> Hadoop Distributed File System - Scala Collections
+ * 
+*/
